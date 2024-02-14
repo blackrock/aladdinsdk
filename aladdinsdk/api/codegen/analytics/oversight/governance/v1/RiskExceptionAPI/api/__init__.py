9e013c0131d159a11cc2f9712658259cf147464c
Vedant Naik <naik.vedant@gmail.com>
2024-02-13 19:58:35 -0500

    codegen for pilot rollout (#5)
    * codegen
    
    * disable codegen in github action's build step

---
+++ aladdinsdk/api/codegen/analytics/oversight/governance/v1/RiskExceptionAPI/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from aladdinsdk.api.codegen.analytics.oversight.governance.v1.RiskExceptionAPI.api.default_risk_exception_api import DefaultRiskExceptionAPI
+
---
+++ aladdinsdk/api/codegen/investment_research/surveillance/criterion/v1/CriterionAPI/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from aladdinsdk.api.codegen.investment_research.surveillance.criterion.v1.CriterionAPI.api.default_criterion_api import DefaultCriterionAPI
+
